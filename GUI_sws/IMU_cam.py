4500394ea5db650710390b9df5afe120c6cd86c2
jehunseo <benbot591@gmail.com>
2020-08-12 10:50:18 +0900

    update/IMU_cam.py

--- GUI_sws/IMU_cam.py
+++ GUI_sws/IMU_cam.py
@@ -18,13 +18,15 @@ flag = 0
 pi = math.pi
 data = []
 RPM = 0
+dist = 0
 def RCV_IMU(s, i):
     t = []
     global flag
     global data
     global RPM
+    global dist
     cnt = 0
-
+    pi = math.pi
     while True:
         if (flag == 0):
     #########################################create csv file
@@ -32,7 +34,7 @@ def RCV_IMU(s, i):
             file_path = ('/home/pi/Desktop/IMU_hex_testing/'+date+'('+str(i)+').csv')
             print("IMU "+str(i)+" start time :" ,date)
             file_ = open(file_path, 'a')
-            file_.write("sequence,EulerR,EulerP,EulerY,GyroX,GyroY,GyroZ,AccelX,AccelY,AccelZ,MagnetX,MagnetY,MagnetZ, RPM\r\n")
+            file_.write("sequence,EulerR,EulerP,EulerY,GyroX,GyroY,GyroZ,AccelX,AccelY,AccelZ,MagnetX,MagnetY,MagnetZ,RPM\r\n")
             start = time.time()
             
             flag = 1
@@ -44,6 +46,7 @@ def RCV_IMU(s, i):
 
                     Euler = float(data[4] / 100) * pi / 180
                     timestamp = time.time()
+                    time_ref = timestamp
                     
                     break
 
@@ -58,21 +61,31 @@ def RCV_IMU(s, i):
                 Euler = float(data[4] / 100) * pi / 180
                 timestamp = time.time()
                 
-                if Euler * Euler_ < -4: 
+                
+                if Euler * Euler_ < -1:
                     t.append(timestamp_)
-
-                if len(t) >= 2 :
+                '''
+                if Euler * Euler_ < -8.8 :
+                    Tp = (timestamp_ - time_ref) + (pi - Euler) * (timestamp - timestamp_) / (Euler_ + Euler)
+                    RPM = 60 / Tp
+                    time_ref = timestamp
+                '''
+                if len(t) == 2 : # time period during one rotation
                     RPM = 60 / (t[1] - t[0])
+                    
+                    dist = dist + pi*2*0.305
                     t.remove(t[0])
+                
                 '''
                 diff = Euler - Euler_
                 if diff == 0.0 : continue
 
-                Tp = timestamp + ((pi - Euler) * (timestamp - timestamp_)) / (diff)
+                Tp = timestamp + (((pi - Euler) * (timestamp - timestamp_)) / (diff))
                 RPM = 60 / Tp
 
                 print(RPM)
                 '''
+                
                 file_.write("%d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %d\r\n"%(cnt, data[2]/100, data[3]/100, data[4]/100, data[5]/10, data[6]/10, data[7]/10, data[8]/1000, data[9]/1000, data[10]/1000, data[11]/10, data[12]/10, data[13]/10, RPM))
             cnt = cnt + 1
         
@@ -109,25 +122,13 @@ def RCV_cap():
             flag = 3
 #            delay = delay + 5
             
-
 def GUI():
     global data_flag
-
     global count
     root = Tk() #
     root.title('MHE_MeasureWindow')
     root.geometry('720x456+0+0')
     root.resizable(False, False)
-
-    '''
-    Imgobj_logo = PhotoImage(file = "./MHE.png")
-    Img_logo = Label(root, image=Imgobj_logo)
-    Img_logo.place(anchor = 'ne', relx = 1, rely=0)
-
-    Imgobj_wheel = PhotoImage(file = "./_vehicle.png")
-    Img_wheel = Label(root, image = Imgobj_wheel, width = 500, height = 258)
-    Img_wheel.place(anchor = 'sw', relx = 0.05, rely = 0.9)
-    '''
 
     background_image=PhotoImage(file = "_bg.png")
     background_label = Label(root, image=background_image)
@@ -139,50 +140,37 @@ def GUI():
     fontstyle = tkFont.Font(family = 'Courier', size = 12)
 
     lbl_accelx = Label(frame, text = "Accel_X", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_accelx.grid(row = 0, column = 0)
+    lbl_accelx.grid(row = 2, column = 0)
     lbl_accely = Label(frame, text = "Accel_Y", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_accely.grid(row = 0, column = 1)
+    lbl_accely.grid(row = 2, column = 1)
     lbl_accelz = Label(frame, text = "Accel_Z", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_accelz.grid(row = 0, column = 2)
+    lbl_accelz.grid(row = 2, column = 2)
     lbl_gyrox = Label(frame, text = "Gyro_X", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_gyrox.grid(row = 2, column = 0)
+    lbl_gyrox.grid(row = 4, column = 0)
     lbl_gyroy = Label(frame, text = "Gyro_Y", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_gyroy.grid(row = 2, column = 1)
+    lbl_gyroy.grid(row = 4, column = 1)
     lbl_gyroz = Label(frame, text = "Gyro_Z", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_gyroz.grid(row = 2, column = 2)
-    '''
-    lbl_velx = Label(frame, text = "Velocity_X", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_velx.grid(row = 4, column = 0)
-    lbl_vely = Label(frame, text = "Velocity_Y", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_vely.grid(row = 4, column = 1)
-    lbl_velz = Label(frame, text = "Velocity_Z", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_velz.grid(row = 4, column = 2)
-    lbl_rotcnt = Label(frame, text = "Rot.count", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_rotcnt.grid(row = 6, column = 1)
-    '''
+    lbl_gyroz.grid(row = 4, column = 2)
+    lbl_rotcnt = Label(frame, text = "distance", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
+    lbl_rotcnt.grid(row = 0, column = 0)
     lbl_Dist = Label(frame, text = "RPM", font = fontstyle, pady=2, relief = 'solid', borderwidth = 1, width = 10, background = 'white')
-    lbl_Dist.grid(row = 4, column = 2)
+    lbl_Dist.grid(row = 0, column = 1)
 
     var = []
     lbl_txt = []
     
-    for iter in range(7) :
+    for iter in range(8) :
         var.append(StringVar())
         lbl_txt.append(Label(frame, textvariable = var[-1], font = fontstyle, pady=2, background = 'white'))
     
-    lbl_txt[0].grid(row = 1, column = 0)
-    lbl_txt[1].grid(row = 1, column = 1)
-    lbl_txt[2].grid(row = 1, column = 2)
-    lbl_txt[3].grid(row = 3, column = 0)
-    lbl_txt[4].grid(row = 3, column = 1)
-    lbl_txt[5].grid(row = 3, column = 2)
-    lbl_txt[6].grid(row = 5, column = 2)
-    '''
-    lbl_txt[7].grid(row = 5, column = 1)
-    lbl_txt[8].grid(row = 5, column = 2)
-    lbl_txt[9].grid(row = 7, column = 1)
-    lbl_txt[10].grid(row = 7, column = 2)
-    '''
+    lbl_txt[0].grid(row = 3, column = 0)
+    lbl_txt[1].grid(row = 3, column = 1)
+    lbl_txt[2].grid(row = 3, column = 2)
+    lbl_txt[3].grid(row = 5, column = 0)
+    lbl_txt[4].grid(row = 5, column = 1)
+    lbl_txt[5].grid(row = 5, column = 2)
+    lbl_txt[6].grid(row = 1, column = 0)
+    lbl_txt[7].grid(row = 1, column = 1)
     '''
     var list : 0 ~ 10
     accelx
@@ -199,7 +187,8 @@ def GUI():
 
     '''
     while flag == 1 :
-        if type(data) != type((1,)) : continue
+        if type(data) != type((1,)) or len(data) != 17  : continue
+
         var[0].set(data[8]/1000)
         var[1].set(data[9]/1000)
         var[2].set(data[10]/1000)
@@ -207,7 +196,9 @@ def GUI():
         var[3].set(data[5]/10)
         var[4].set(data[6]/10)
         var[5].set(data[7]/10)
-        var[6].set(int(RPM))
+        var[6].set(round(dist, 3))
+        var[7].set(int(RPM))
+
         frame.update()
 ###########################################################################start
 threads = []
@@ -247,5 +238,3 @@ for iter in range(len(threads)) :
 
 for iter in range(len(threads)) :
     threads[iter].join()
-
-
