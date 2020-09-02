68bc50babd95f0ceea7b87a43e6b2ac666ab9151
youniechoi <youniechoi0@gmail.com>
2020-09-02 17:10:06 +0900

    sws_E2box_dual_ver0

--- sws_E2box_dual.py
+++ sws_E2box_dual.py
@@ -23,7 +23,6 @@ threads = []
 ###########################################################################
 def RCV_IMU(s, i):
     t = []
-    flag.value=0
     global data
     global RPM
     global dist
@@ -34,88 +33,73 @@ def RCV_IMU(s, i):
         if (flag[i] == 0):
             #create csv file
             date = datetime.now().strftime('%y%m%d_%H%M%S')
-            file_path = ('/media/pi/869EE7369EE71E05/'+date+'('+str(i)+').csv')
+            file_path = ('/media/pi/728EB4FE8EB4BC43/'+date+'('+str(i)+').csv')
             print("IMU "+str(i)+" start time :" ,date)
             file_ = open(file_path, 'a')
-            file_.write("sequence,EulerR,EulerP,EulerY,GyroX,GyroY,GyroZ,AccelX,AccelY,AccelZ,MagnetX,MagnetY,MagnetZ,RPM,time\r\n")
+            file_.write("sequence,GyroX,GyroY,GyroZ,AccelX,AccelY,AccelZ,MagnetX,MagnetY,MagnetZ\r\n")
             start = time.time()
             flag[i] = 1
 
-            if (i == 0) :
-                print(str(i) + "camera start")
-                camera = PiCamera()
-                camera.resolution = (640, 480)
-                camera.framerate = 10
-               # camera.start_preview(fullscreen = false, window = (-320,0,640,480))
-                camera.start_recording('/media/pi/869EE7369EE71E05/'+ date +'.h264')
-
-            
+        
         # IMU records data to .csv
         if (flag[i] == 1):
-         #   print(flag)           
             data = s.read_until( b'UU')
 #            print(len(data))
 #            continue
             if len(data) == 34:
                 data = struct.unpack('>BBhhhhhhhhhhhhhHhh', data)                    
-  #              file_.write("%d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %d, %.3f\r\n"%(cnt, data[2]/100, data[3]/100, data[4]/100, data[5]/10, data[6]/10, data[7]/10, data[8]/1000, data[9]/1000, data[10]/1000, data[11]/10, data[12]/10, data[13]/10, time.time() - start))
+                file_.write("%d, %f, %f, %f, %f, %f, %f, %f, %f, %f\r\n"%(cnt, data[5]/10, data[6]/10, data[7]/10, data[8]/1000, data[9]/1000, data[10]/1000, data[11]/10, data[12]/10, data[13]/10))
             
             cnt = cnt + 1
-            
-            if (i == 0) :
-                print(str(i)+" count: ",cnt, end='\r')
-        
+    
             
         if (time.time() - start)>=300: # 5min 300, 1min 60 #cnt >= 300000: after 5 min (1000Hz receiving)
+            print(str(cnt)+ ","+str(cnt/3000) +"%"  + " IMU " + str(i) + " end : ", time.time() - start)
             flag[i] = 2            
         # if record end, initialize variables and return to start
         if (flag[i] == 2):
-            print(str(cnt)+ ","+str(cnt/3000) +"%"  + " IMU " + str(i) + " end : ", time.time() - start)
-            
-            if (i == 0):
-                print("camera end")
-                camera.stop_recording()
-                camera.close()
-              #  camera.stop_preview()
+            print(f'flag[{i}]')
 
 
-            flag[i] = 0
+        if(flag[0] == 2 and flag[1] == 2):
+            flag[0] = 0
+            flag[1] = 0
             cnt = 0
 
         
 def RCV_cam():
-    '''
+
     camera = PiCamera()
     camera.resolution = (640, 480)
-    camera.framerate = 10
-    camera.start_preview()
-    date = datetime.now().strftime('%y%m%d_%H%M%S')
-    file_path = ('/home/pi/Documents/dat/'+date+'('+str(i)+').h264')
+    camera.framerate = 40
+    flag_cam = 0
+    print(flag[0])
+    
+    try:
+        while True:
+    #        if flag == 0 :
 
-    camera.start_recording(file_path)
-    '''
-    while True:
-#        if flag == 0 :
+            if (flag[0] == 1 and flag_cam == 0):
+                print("camera start")
+                start = time.time()
+                camera.start_preview()
+                camera.start_recording('/media/pi/728EB4FE8EB4BC43/'+datetime.now().strftime('%y%m%d_%H%M%S')+'.h264')
+                flag_cam = 1
+                
+            if (flag[0] == 2) :
+                print("cam end :", time.time() - start)
+                camera.stop_recording()
+                flag_cam = 0
 
-        if (flag[0] ==1):
-            print("camera start")
-            camera = PiCamera()
-            camera.resolution = (640, 480)
-            camera.framerate = 10
-            camera.start_preview()
-            camera.start_recording('/media/pi/869EE7369EE71E05/'+datetime.now().strftime('%y%m%d_%H%M%S')+'.h264')
-
-        if (flag[0] == 2) :
-            print("cam end :", time.time() - start)
-            camera.stop_recording()
-            camera.stop_preview()
+#                 camera.stop_preview()
+#                camera.close()
+    except KeyboardInterrupt:
+        camera.close()
     
 ########################### main #################################
 # determine USBserial device
 port_result = []
 ports = glob.glob('/dev/ttyUSB*')
-
-
 
 for port in ports:
     print(port)
@@ -131,7 +115,7 @@ for port in ports:
 ser_ = []
 for port in port_result:
     ser_.append(serial.Serial(port, 921600))
-    print(ser_[-1])
+#    print(ser_[-1])
 
 # Check all sensor on
 while True:
@@ -139,10 +123,12 @@ while True:
        if ( s.read(size = 1)) : cnt = cnt + 1
     if cnt == len(ser_): break
 
+
 for iter in range(len(ser_)):
    threads.append(Process(target = RCV_IMU, args = (ser_[iter], iter)))
 
-#threads.append(Process(target = RCV_cam))
+threads.append(Process(target = RCV_cam))
+
 
 print(threads)
 
